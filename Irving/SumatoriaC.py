8174978f0894863f5cdf65db473c8247fa0b8293
IrvingC16 <212T0368@itsm.edu.mx>
2023-11-14 11:43:09 -0600

    Una suma

---
+++ Irving/SumatoriaC.py
@@ -0,0 +1,9 @@
+# Definir dos números
+numero1 = 5
+numero2 = 7
+
+# Sumar los dos números
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de {} y {} es: {}".format(numero1, numero2, suma))
