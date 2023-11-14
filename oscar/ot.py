f6a631634c2f37854b29133a5b82356ddffe7deb
OscarGarcia1124 <zamorazh1234@gmail.com>
2023-11-14 11:40:32 -0600

    feature: Oscar

---
+++ oscar/ot.py
@@ -0,0 +1,17 @@
+def suma(a, b):
+    resultado = a + b
+    return resultado
+
+def resta(a, b):
+    resultado = a - b
+    return resultado
+
+# Ejemplos de uso
+numero1 = 10
+numero2 = 5
+
+resultado_suma = suma(numero1, numero2)
+resultado_resta = resta(numero1, numero2)
+
+print(f"La suma de {numero1} y {numero2} es: {resultado_suma}")
+print(f"La resta de {numero1} y {numero2} es: {resultado_resta}")
