4974b94dadcb317c57f4a5b312d15081a7e3a202
bryanviveros <bryan.viveros.2003@gmial.com>
2023-11-14 12:00:34 -0600

    Subi un py a mi rama

--- bryan/hola.py
+++ bryan/hola.py
@@ -1,1 +1,12 @@
-print("Hola, mundo!")
+# Definir una lista de números
+numeros = [1, 2, 3, 4, 5]
+
+# Inicializar la variable para la sumatoria
+sumatoria = 0
+
+# Calcular la sumatoria usando un bucle for
+for numero in numeros:
+    sumatoria += numero
+
+# Imprimir el resultado
+print("La sumatoria es:", sumatoria)
